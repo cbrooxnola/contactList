fcab397877eec4c1500e2144f82f0758274c9fea
Christopher <csbroox@gmail.com>
2023-09-07 13:49:56 -0500

    ready for test run

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 import { useState } from 'react'
-
-import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] =useState([]);
+  const [hash, setHash] = useState(window.location.hash.slice(1)*1);
+
+  useEffect (() => {
+    const fetchUsers = async () => {
+    const response = await fetch('https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users');
+    const json = await response.json();
+    setUsers(json);
+  }
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('hashchange', () => {
+      setHash(window.location.hash.slice(1)*1);
+    });
+  }, []);
+
+  const user = users.find( user => hash === user.id);
 
   return (
     <>
-      <div>
-        
-      </div>
+    <h1>Contact List ({ users.length })</h1>
+    { user ? <p>user.address</p>: null }
+    <ul>
+      {
+        users.map( user =>{
+          return (
+            <li key={user.id} className={ user.id === hash ? 'selected': ''}>
+              <a href={`#${user.id === hash ? '': user.id}`}>
+              {user.name}
+              </a>
+            </li>
+          );
+        })
+      }
+    </ul>
+    <hr />
+      
       
       
     </>
